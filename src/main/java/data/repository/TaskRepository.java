c0af453587303b64534e5d32a5df762145b82a5c
Alessia-Nicolini <alessia.nicolini2005@gmail.com>
2025-11-04 15:02:55 +0100

    Create TakRepository

--- src/main/java/data/repository/TaskRepository.java
+++ src/main/java/data/repository/TaskRepository.java
@@ -1,4 +1,36 @@
 package data.repository;
 
-public class TaskRepository {
+import data.model.Task;
+import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.bson.types.ObjectId;
+
+import java.io.Serializable;
+import java.util.List;
+
+@ApplicationScoped
+public class TaskRepository implements PanacheMongoRepository<Task> {
+    public Task findById(ObjectId id) {
+        return find("_id", id).firstResult();
+    }
+
+    public List<Task> findByFase(String fase) {
+        return find("fase", fase).list();
+    }
+
+    public List<Task> findByEtichetta(String etichetta) {
+        return find("etichette", etichetta).list();
+    }
+
+    public List<Task> findByAssegnatario(String assegnatario) {
+        return find("assegnatari", assegnatario).list();
+    }
+
+    public List<Task> findByScadenza(String scadenza) {
+        return find("data_scadenza", scadenza).list();
+    }
+
+    public List<Task> findByProgetto(ObjectId idProgetto) {
+        return find("idProgetto", idProgetto).list();
+    }
 }
