d72275ae82562e0e89508cef74dc576d25f8411d
Alessia-Nicolini <alessia.nicolini2005@gmail.com>
2025-11-04 16:42:21 +0100

    Create TaskResource

--- src/main/java/web/resources/TaskResource.java
+++ src/main/java/web/resources/TaskResource.java
@@ -4,59 +4,64 @@ import data.model.Task;
 import data.repository.TaskRepository;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.bson.types.ObjectId;
-
+import service.TaskService;
 import java.util.Date;
 import java.util.List;
 
 @Path("/task")
 public class TaskResource {
+    private final TaskService taskService;
     private final TaskRepository taskRepository;
 
-    public TaskResource(TaskRepository taskRepository) {
+    public TaskResource(TaskService taskService, TaskRepository taskRepository) {
+        this.taskService = taskService;
         this.taskRepository = taskRepository;
     }
 
+    @Path("/create")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Task createTask(Task task) {
-        taskRepository.persistOrUpdate(task);
-        return task;
+    public Response createTask(Task task) {
+        taskService.createTask(task);
+        return Response.ok().build();
     }
+
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Task> getAllTasks() {
-        return taskRepository.findAll().list();
+        return taskService.getAllTasks();
     }
 
     @GET
     @Path("/fase/{fase}")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Task> getTasksByFase (@PathParam("fase") String fase) {
-        return taskRepository.findByFase(fase);
+        return taskService.getTasksByFase(fase);
     }
 
     @GET
     @Path("/etichetta/{etichetta}")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Task> getTasksByEtichetta (@PathParam("etichetta") String etichetta) {
-        return taskRepository.findByEtichetta(etichetta);
+        return taskService.getTasksByEtichetta(etichetta);
     }
 
     @GET
     @Path("/assegnatario/{assegnatario}")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Task> getTasksByAssegnatario (@PathParam("assegnatario") String assegnatario) {
-        return taskRepository.findByAssegnatario(assegnatario);
+        return taskService.getTasksByAssegnatario(assegnatario);
     }
 
     @GET
     @Path("/scadenza/{scadenza}")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Task> getTasksByScadenza (@PathParam("scadenza") Date scadenza) {
-        return taskRepository.findByScadenza(scadenza);
+        return taskService.getTasksByScadenza(scadenza);
     }
 
     @GET
@@ -64,13 +69,13 @@ public class TaskResource {
     @Produces(MediaType.APPLICATION_JSON)
     public List<Task> getTasksByProgetto (@PathParam("progetto") String progettoId) {
         ObjectId progetto = new ObjectId(progettoId);
-        return taskRepository.findByProgetto(progetto);
+        return taskService.getTasksByProgetto(progetto);
     }
 
     @DELETE
     @Path("/{id}")
     public void deleteTask(@PathParam("id") String id) {
         ObjectId key = new ObjectId(id);
-        taskRepository.deleteById(key);
+        taskService.deleteTask(key);
     }
 }
