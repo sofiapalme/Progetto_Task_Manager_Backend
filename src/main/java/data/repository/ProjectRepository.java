b7ba44bae9983358d4119f57b520dae4c8399e13
fabioporzio <fabio.porzio00@gmail.com>
2025-11-04 16:53:33 +0100

    Created test API to find projects by user

--- src/main/java/data/repository/ProjectRepository.java
+++ src/main/java/data/repository/ProjectRepository.java
@@ -1,4 +1,22 @@
 package data.repository;
 
-public class ProjectRepository {
+import data.model.Project;
+import data.model.Task;
+import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@ApplicationScoped
+public class ProjectRepository implements PanacheMongoRepository<Project> {
+
+    public List<Project> getAllProjectsByUser(ObjectId id) {
+        return find("collaboratori.idUser in ?1", List.of(id)).list();
+    }
+
+
 }
