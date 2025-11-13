cf101f849847c5000bb3baf176483994387627ed
Alessia-Nicolini <alessia.nicolini2005@gmail.com>
2025-11-13 09:41:37 +0100

    Correct permissions on TaskResource

--- src/main/java/web/resources/TaskResource.java
+++ src/main/java/web/resources/TaskResource.java
@@ -1,7 +1,7 @@
 package web.resources;
-
 import data.model.Task;
-import data.repository.TaskRepository;
+import jakarta.annotation.security.DenyAll;
+import jakarta.annotation.security.PermitAll;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -10,16 +10,16 @@ import service.TaskService;
 import java.util.Date;
 import java.util.List;
 
+@DenyAll
 @Path("/task")
 public class TaskResource {
     private final TaskService taskService;
-    private final TaskRepository taskRepository;
 
-    public TaskResource(TaskService taskService, TaskRepository taskRepository) {
+    public TaskResource(TaskService taskService) {
         this.taskService = taskService;
-        this.taskRepository = taskRepository;
     }
 
+    @PermitAll
     @Path("/create")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
@@ -29,55 +29,61 @@ public class TaskResource {
         return Response.ok().build();
     }
 
-
+    @PermitAll
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllTasks() {
-        taskService.getAllTasks();
-        return Response.ok().build();
+        List<Task> taskList = taskService.getAllTasks();
+        return Response.ok(taskList).build();
     }
 
+    @PermitAll
     @GET
     @Path("/fase/{fase}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getTasksByFase (@PathParam("fase") String fase) {
-        taskService.getTasksByFase(fase);
-        return Response.ok().build();
+        List<Task> taskFaseList = taskService.getTasksByFase(fase);
+        return Response.ok(taskFaseList).build();
     }
 
+    @PermitAll
     @GET
     @Path("/etichetta/{etichetta}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getTasksByEtichetta (@PathParam("etichetta") String etichetta) {
-        taskService.getTasksByEtichetta(etichetta);
-        return Response.ok().build();
+        List<Task> taskEtichettaList = taskService.getTasksByEtichetta(etichetta);
+        return Response.ok(taskEtichettaList).build();
     }
 
+    @PermitAll
     @GET
     @Path("/assegnatario/{assegnatario}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getTasksByAssegnatario (@PathParam("assegnatario") String assegnatario) {
-        taskService.getTasksByAssegnatario(assegnatario);
-        return Response.ok().build();
+        List<Task> taskAssegnatarioList = taskService.getTasksByAssegnatario(assegnatario);
+        return Response.ok(taskAssegnatarioList).build();
     }
 
+    @PermitAll
     @GET
     @Path("/scadenza/{scadenza}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getTasksByScadenza (@PathParam("scadenza") Date scadenza) {
-        taskService.getTasksByScadenza(scadenza);
-        return Response.ok().build();
+        List<Task> taskScadenzaList = taskService.getTasksByScadenza(scadenza);
+        return Response.ok(taskScadenzaList).build();
     }
 
+    @PermitAll
     @GET
     @Path("/progetto/{progetto}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getTasksByProgetto (@PathParam("progetto") String progettoId) {
         ObjectId progetto = new ObjectId(progettoId);
-        taskService.getTasksByProgetto(progetto);
-        return Response.ok().build();
+        List<Task> taskProgettoList = taskService.getTasksByProgetto(progetto);
+        return Response.ok(taskProgettoList).build();
     }
 
+    @PermitAll
     @DELETE
     @Path("/{id}")
     public void deleteTask(@PathParam("id") String id) {
