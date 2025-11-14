359e542e9bd98a48ab8ce31fc969e3fe222a5628
Sofia Palmeri <sofia.palmeri@itsincom.it>
2025-11-14 20:23:28 +0100

    Created AuthorizationService class and implemented canDeleteProject method

---
+++ src/main/java/service/AuthorizationService.java
@@ -0,0 +1,30 @@
+package service;
+
+import data.model.Project;
+import data.model.User;
+import data.repository.ProjectRepository;
+import data.repository.UserRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.bson.types.ObjectId;
+
+@ApplicationScoped
+public class AuthorizationService {
+    private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
+
+    public AuthorizationService(ProjectRepository projectRepository, UserRepository userRepository) {
+        this.projectRepository = projectRepository;
+        this.userRepository = userRepository;
+    }
+
+    public boolean canDeleteProject(ObjectId projectId, String email) {
+        Project project = projectRepository.findById(projectId);
+        User creatore = project.getCreatore();
+        User utenteLoggato = userRepository.findByEmail(email);
+
+        if (creatore == null) {
+            return false;
+        }
+        return creatore.getId().equals(utenteLoggato.getId());
+    }
+}
